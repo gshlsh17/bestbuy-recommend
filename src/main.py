c745fe9f29859b8f8da79350bc0bb29d35673bab
shqyking <sunqyoung@gmail.com>
2015-05-13 14:16:30 -0400

    add content-based filtering

--- src/main.py
+++ src/main.py
@@ -5,6 +5,10 @@ from collections import defaultdict
 from Parser import *
 import operator
 from preprocess import *
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy
+from numpy import *
+from heapq import *
 
 def getQuerySkuDict(filename):
     inputFile = open(filename)
@@ -18,36 +22,94 @@ def getQuerySkuDict(filename):
     	querySkuDict[query][sku] += 1
     return querySkuDict	
 
+def getProductInfo():
+    productTree = ET.parse('../data/small_product_data.xml')
+    root = productTree.getroot()
+    productSkus = []
+    productDescpt = []
+    for product in root.findall('product'):
+        sku = product.find('sku').text
+        description = product.find('longDescription').text
+        productSkus.append(sku)
+        productDescpt.append(description)
+    return productSkus, productDescpt
+
+def getDatumInfo(testFileName):    
+    testFile = open(testFileName, 'r')
+    reader = csv.reader(testFile)
+    datum = []
+    queries = []
+    for row in reader:
+        datum.append(row)
+        queries.append(row[3])
+    return datum, queries
+
 #if __name__ == "__main__":
-# trainFileName = '../data/psudo-train.csv'
-# testFileName = '../data/psudo-test.csv'
-# querySkuDict = getQuerySkuDict(trainFileName)
-# if we handle with typos
-queryFreq = getQueryFrequency()
-sortedQuery = sorted(queryFreq.items(), key=operator.itemgetter(1), reverse=True)
-spellDict = spellChecker(sortedQuery)
-sortedDict = sorted(spellDict.items(), key=operator.itemgetter(0))
+trainFileName = '../data/psudo_train2.csv'
+testFileName = '../data/psudo_test2.csv'
+querySkuDict = getQuerySkuDict(trainFileName)
 
-testFile = open(testFileName, 'r')
+productSkus, productDescpt = getProductInfo()
+datum, queries = getDatumInfo(testFileName)
+corpus = []
+corpus.extend(productDescpt)
+corpus.extend(queries)
+
+vectorizer = TfidfVectorizer(min_df=1)
+tfidf = vectorizer.fit_transform(corpus)
+tfidfArray = tfidf.toarray()
+numProducts = len(productSkus)
+numQuery = len(datum)
+"""
+for each query, I get the top five sku with product(tfidf(product), tfidf(query))
+"""
+total = 0.0
+correct = 0.0
+tmp = 0.0
+for x in range(0, numQuery):
+    total += 1
+    trueSku = datum[x][1]
+    query = queries[x]
+    #first do content-based filtering
+    queryArray = tfidfArray[x+numProducts,:]
+    scoreDict = {}
+    for j in range(0, numProducts):
+        prodArray = tfidfArray[j,:]
+        score = numpy.dot(queryArray, prodArray)
+        if score > 0:
+            scoreDict[productSkus[j]] = score
+    skusItems = nlargest(5, scoreDict.items(), key=operator.itemgetter(1))
+    skus1 = []
+    for sku in skusItems:
+        skus1.append(sku[0])
+    #then do collaborative filtering
+    skus2 = []
+    if query in querySkuDict:
+        for sku in sorted( querySkuDict[query], key=querySkuDict[query].get, reverse = True ):
+            skus2.append(sku)
+        skus2 = skus2[0:5]
+    if trueSku in skus2:
+        correct += 1.0 / (skus2.index(trueSku) + 1)
+    if trueSku in skus1:
+        tmp += 1.0 / (skus1.index(trueSku) + 1)
+
+
+
+
+testFile = open(testFileName)
 reader = csv.reader(testFile)
-par = Parser()
-total = 0
-correct = 0
-
+total = 0.0
+correct = 0.0
 for row in reader:
 	skus = []
-	query = row[3] #need to modifed for true test file
-	query = preprocessQuery(query, par) # get a new string, stemmed, ordered
+	query = row[3] # need to modifed for true test file
 	total += 1
 	if query in querySkuDict:
 		for sku in sorted( querySkuDict[query], key=querySkuDict[query].get, reverse = True ):
 			skus.append(sku)
 		skus = skus[0:5]
 		if row[1] in skus:
-			print 'Yes'
 			correct += 1
-		else:
-			print 'No'
 
 
 
